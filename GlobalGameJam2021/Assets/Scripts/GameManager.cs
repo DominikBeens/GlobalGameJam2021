0233f592c8ba59328f307584909314d5fbd8bb04
Dominik Beens <dominik.beens@hotmail.nl>
2021-01-29 19:22:03 +0100

    scene stuff

--- GlobalGameJam2021/Assets/Scripts/GameManager.cs
+++ GlobalGameJam2021/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager> {
 
     [SerializeField] private string sceneToLoad = "Menu";
+
+    [SerializeField] private string gameScene = "Game";
 
     protected override void Awake() {
         base.Awake();
@@ -15,5 +18,14 @@ public class GameManager : Singleton<GameManager> {
     private IEnumerator Initialize() {
         yield return new WaitForSeconds(0.1f);
         SceneManager.LoadScene(sceneToLoad);
+        yield return new WaitForSeconds(0.1f);
+        yield return LoadGame();
+    }
+
+    private IEnumerator LoadGame() {
+        yield return null;
+        SceneManager.LoadScene(gameScene);
+        yield return new WaitForSeconds(0.1f);
+        BoardManager.Instance.BuildLevel(10, 10, new List<EntityPlacement>());
     }
 }
