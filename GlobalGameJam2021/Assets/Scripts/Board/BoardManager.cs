9978b3a522c86d5a0c6fc4dce47c96e9881b0ced
Han Hakkenberg <han.hakkenberg@gmail.com>
2021-01-29 19:14:40 +0100

    Tile Added

--- GlobalGameJam2021/Assets/Scripts/Board/BoardManager.cs
+++ GlobalGameJam2021/Assets/Scripts/Board/BoardManager.cs
@@ -2,35 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardManager : MonoBehaviour
+public class BoardManager : Singleton<GameManager>
 {
-    private static BoardManager instance;
+    [SerializeField] private GameObject tile;
+    private Tile[,] board;
 
-    public static BoardManager Instance
+    protected override void Awake()
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<BoardManager>();
-            }
-
-            return instance;
-        }
-    }
-
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-
-
+        base.Awake();
         DontDestroyOnLoad(gameObject);
     }
+
+    private void Start()
+    {
+        BuildLevel(10, 10, new List<EntityPlacement>());
+    }
+
+    public void BuildLevel(int sizeX, int sizeY, List<EntityPlacement> entitiesToPlace)
+    {
+        board = new Tile[sizeX, sizeY];
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                board[x, y] = Instantiate(tile).GetComponent<Tile>();
+                board[x, y].gameObject.transform.position = new Vector3(x, y, 0);
+            }
+        }
+    }
+}
+
+public struct EntityPlacement
+{
+    public Vector2 position;
+
 }
