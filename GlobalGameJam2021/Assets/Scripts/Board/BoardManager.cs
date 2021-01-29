5347856f35f187570ff50043a8d5237c4cce9699
Han Hakkenberg <han.hakkenberg@gmail.com>
2021-01-29 23:48:09 +0100

    Tile Highlight

--- GlobalGameJam2021/Assets/Scripts/Board/BoardManager.cs
+++ GlobalGameJam2021/Assets/Scripts/Board/BoardManager.cs
@@ -7,7 +7,7 @@ public class BoardManager : Singleton<BoardManager>
     public List<EntityPlacement> Level = new List<EntityPlacement>();
     [SerializeField] private GameObject tile;
     private Tile[,] board;
-
+    public PlayerEntity testTest;
     protected override void Awake()
     {
         base.Awake();
@@ -28,7 +28,8 @@ public class BoardManager : Singleton<BoardManager>
 
         for (int i = 0; i < entitiesToPlace.Count; i++)
         {
-            Instantiate(entitiesToPlace[i].MyEntity, entitiesToPlace[i].position, entitiesToPlace[i].MyEntity.transform.rotation);
+            testTest = Instantiate(entitiesToPlace[i].MyEntity, entitiesToPlace[i].position, entitiesToPlace[i].MyEntity.transform.rotation).GetComponent<PlayerEntity>();
+            SelectMoveSpots(testTest);
         }
     }
 
@@ -63,11 +64,46 @@ public class BoardManager : Singleton<BoardManager>
             board[xNumber, z].FlipTile();
         }
     }
+
+    public void SelectMoveSpots(PlayerEntity toUse)
+    {
+        SelectDirection(toUse.MoveActionData.North, toUse.gameObject.transform.position, toUse.North.position);
+        SelectDirection(toUse.MoveActionData.East, toUse.gameObject.transform.position, toUse.East.position);
+        SelectDirection(toUse.MoveActionData.South, toUse.gameObject.transform.position, toUse.South.position);
+        SelectDirection(toUse.MoveActionData.West, toUse.gameObject.transform.position, toUse.West.position);
+        SelectDirection(toUse.MoveActionData.NorthEast, toUse.gameObject.transform.position, toUse.NorthEast.position);
+        SelectDirection(toUse.MoveActionData.NorthWest, toUse.gameObject.transform.position, toUse.NorthWest.position);
+        SelectDirection(toUse.MoveActionData.SouthEast, toUse.gameObject.transform.position, toUse.SouthEast.position);
+        SelectDirection(toUse.MoveActionData.SouthWest, toUse.gameObject.transform.position, toUse.SouthWest.position);
+    }
+
+    private void SelectDirection(int Amount, Vector3 currentPos, Vector3 directionPos)
+    {
+        for (int i = 0; i < Amount; i++)
+        {
+            Vector3 pos = (currentPos - directionPos);
+            Vector2 dir = new Vector2(directionPos.x, directionPos.z);
+            SelectTile(dir - new Vector2(pos.x, pos.z) * i);
+
+
+        }
+    }
+
+    private void SelectTile(Vector2 toSelect)
+    {
+        int x = Mathf.RoundToInt(toSelect.x);
+        int y = Mathf.RoundToInt(toSelect.y);
+        if (x >= 0 && x < board.GetLength(0) && y >= 0 && y < board.GetLength(1))
+        {
+            board[x, y].gameObject.transform.position += new Vector3(0, 0.2f, 0);
+        }
+    }
+
 }
 
 [System.Serializable]
 public struct EntityPlacement
 {
-    public Vector2 position;
+    public Vector3 position;
     public GameObject MyEntity;
 }
