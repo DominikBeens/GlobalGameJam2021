a59f001dcd9ebedb7a95dc519d66a94c3f71d64f
Han Hakkenberg <han.hakkenberg@gmail.com>
2021-01-29 17:54:51 +0100

    Board Base Scripts

---
+++ GlobalGameJam2021/Assets/Scripts/Board/BoardManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private static BoardManager instance;
+
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<BoardManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
