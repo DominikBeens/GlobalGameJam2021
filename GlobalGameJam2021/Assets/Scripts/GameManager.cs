c3467f6d1682964e87c23852c4b32f225349945c
Dominik Beens <dominik.beens@hotmail.nl>
2021-01-29 17:58:23 +0100

    added entry and scene auto load

---
+++ GlobalGameJam2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager> {
+
+    [SerializeField] private string sceneToLoad = "Menu";
+
+    protected override void Awake() {
+        base.Awake();
+        Initialize();
+    }
+
+    private void Initialize() {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
