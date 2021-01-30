0d5ddcf7b8689ee210b08c46ced2b311090dbfa2
Dominik Beens <dominik.beens@hotmail.nl>
2021-01-30 23:14:57 +0100

    debug window

---
+++ GlobalGameJam2021/Assets/Scripts/Utils/DebugWindow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DebugWindow : EditorWindow {
+
+    [MenuItem("Tools/Debug Window")]
+    static void Initialize() {
+        DebugWindow window = (DebugWindow)GetWindow(typeof(DebugWindow));
+        window.Show();
+    }
+
+    private void OnGUI() {
+        if (GUILayout.Button("Mark selection dirty")) {
+            Object[] objects = Selection.objects;
+            foreach (Object obj in objects) {
+                EditorUtility.SetDirty(obj);
+            }
+        }
+
+        if (GUILayout.Button("Save project")) {
+            AssetDatabase.SaveAssets();
+        }
+
+    }
+}
