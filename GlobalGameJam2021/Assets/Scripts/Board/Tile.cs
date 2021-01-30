ddef2b850097c20d8f3cec77aa2b591ed9402142
Dominik Beens <dominik.beens@hotmail.nl>
2021-01-30 01:46:49 +0100

    больше анимации плитки

--- GlobalGameJam2021/Assets/Scripts/Board/Tile.cs
+++ GlobalGameJam2021/Assets/Scripts/Board/Tile.cs
@@ -3,35 +3,76 @@ using DG.Tweening;
 
 public class Tile : MonoBehaviour {
 
-    [SerializeField] private Transform visual;
     public Entity myEntity;
+
+    [SerializeField] private Transform rotationTransform;
+    [SerializeField] private Transform hoverTransform;
+    [SerializeField] private Transform highlightTransform;
     [SerializeField] private Transform randomVisualHolder;
+    [SerializeField] private MeshRenderer tileBaseRenderer;
 
     [Header("Flip")]
     [SerializeField] private float flipDuration = 0.3f;
 
-    public bool isFlipped = false;
+    [Header("Hover")]
+    [SerializeField] private float hoverInDuration = 0.2f;
+    [SerializeField] private float hoverOutDuration = 0.1f;
+
+    [Header("Highlight")]
+    [SerializeField] private float highlightInDuration = 0.2f;
+    [SerializeField] private float highlightOutDuration = 0.1f;
+
+    private bool isFlipped = false;
+    private MaterialPropertyBlock tileBasePropertyBlock;
+    private Tween highlightTween;
 
     private void Awake() {
         foreach (Transform t in randomVisualHolder) {
             t.gameObject.SetActive(false);
         }
         randomVisualHolder.GetChild(Random.Range(0, randomVisualHolder.childCount)).gameObject.SetActive(true);
+
+        tileBasePropertyBlock = new MaterialPropertyBlock();
+        tileBaseRenderer.SetPropertyBlock(tileBasePropertyBlock);
     }
 
     public void FlipTile() {
         isFlipped = !isFlipped;
 
         Vector3 rotation = new Vector3(0, 0, gameObject.transform.localEulerAngles.x - 180);
-        visual.DOKill(true);
-        visual.DOLocalRotate(rotation, flipDuration, RotateMode.LocalAxisAdd).SetEase(Ease.OutBack);
+        rotationTransform.DOKill(true);
+        rotationTransform.DOLocalRotate(rotation, flipDuration, RotateMode.LocalAxisAdd).SetEase(Ease.OutBack);
     }
 
     public void Hover() {
-        transform.position += new Vector3(0, 0.1f, 0);
+        hoverTransform.DOKill();
+        hoverTransform.DOLocalMoveY(0.1f, hoverInDuration);
     }
 
     public void UnHover() {
-        transform.position += new Vector3(0, -0.1f, 0);
+        hoverTransform.DOKill();
+        hoverTransform.DOLocalMoveY(0f, hoverOutDuration);
+    }
+
+    public void Highlight() {
+        highlightTransform.DOKill();
+        highlightTransform.DOLocalMoveY(0.1f, highlightInDuration);
+        TweenTileBaseEmission(Color.red, highlightInDuration);
+    }
+
+    public void UnHighlight() {
+        highlightTransform.DOKill();
+        highlightTransform.DOLocalMoveY(0f, highlightOutDuration);
+        TweenTileBaseEmission(Color.green, highlightOutDuration);
+    }
+
+    private void TweenTileBaseEmission(Color color, float duration) {
+        tileBaseRenderer.GetPropertyBlock(tileBasePropertyBlock);
+        Color c = tileBasePropertyBlock.GetColor("_EmissionColor");
+        highlightTween?.Kill();
+        highlightTween = DOTween.To(() => c, x => c = x, color, duration).OnUpdate(() => {
+            tileBasePropertyBlock.SetColor("_EmissionColor", c);
+            tileBaseRenderer.SetPropertyBlock(tileBasePropertyBlock);
+        });
     }
 }
